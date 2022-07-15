2bc26626f16c974c6c10d00be305a9f39a0a9f7a
fletcherdeal92 <fletcherdeal@gmail.com>
2022-07-14 22:21:20 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Fletcher Deal on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
